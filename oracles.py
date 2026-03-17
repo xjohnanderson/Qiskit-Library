47980fae578962940aab4ed70528145ab78ea2f6
xjohnanderson <xjohnanderson@proton.me>
2026-03-17 17:13:04 -0400

    Refactor phase flip oracle and add balanced oracle
    Renamed and refactored the phase flip oracle function, removing unnecessary comments and adding a new balanced oracle function.

--- oracles.py
+++ oracles.py
@@ -1,28 +1,17 @@
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import ZGate
 
-def build_phase_flip_oracle(num_qubits, target_state):
-    """
-    Creates a phase oracle that flips the sign of the target_state.
-    """
+# Phase Oracle
+def phase_flip_oracle(num_qubits, target_state):
     qc = QuantumCircuit(num_qubits)
-    
-    # Identify which qubits are '0' and need an X-gate to match the target
-    # In this example, we assume target_state is a binary string '111'
-    # To target a state like '011', you'd apply X to q0 before the MCZ
-    
-    # Multi-controlled Z gate
-    # For a 3-qubit system, this is an MCZ with 2 controls and 1 target
     mcz = ZGate().control(num_qubits - 1)
     qc.append(mcz, range(num_qubits))
     
     return qc
 
+# Balanced Oracle
 def balanced_oracle():
     qc = QuantumCircuit(2)
     qc.cx(0, 1)  # Performs |x>|y ^ x>
     return qc
 
-# Example usage for 3 qubits targeting state |111>
-oracle = build_phase_flip_oracle(3, '111')
-print(oracle.draw())
